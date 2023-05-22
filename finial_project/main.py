fca146bbf2da7181a15a2c96d19c4def0b45d5fd
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-23 05:04:25 +0900

    [Feat] Finial Project : Resize Read Image

--- finial_project/main.py
+++ finial_project/main.py
@@ -2,9 +2,12 @@ import numpy as np
 import cv2
 
 image_count = 310
+width = 120
+height = 150
 
 image_files = []
 
 for i in range(image_count):
     image = cv2.imread(f"./face_img/train/train{i:03d}.jpg", cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (width, height))
     image_files.append(image)
