7884079bfbd23bc9af6460150ce06c5f29b75cb6
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-23 05:03:20 +0900

    [Feat] Finial Project : Read Image

--- finial_project/main.py
+++ finial_project/main.py
@@ -1,2 +1,10 @@
 import numpy as np
 import cv2
+
+image_count = 310
+
+image_files = []
+
+for i in range(image_count):
+    image = cv2.imread(f"./face_img/train/train{i:03d}.jpg", cv2.IMREAD_GRAYSCALE)
+    image_files.append(image)
