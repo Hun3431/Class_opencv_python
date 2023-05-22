edd85f4633b1feed19885590cad08be365f7a433
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-23 04:56:50 +0900

    [Feat] Chap12 Coin Individual Detection

--- chap12/find.coin.py
+++ chap12/find.coin.py
@@ -15,6 +15,15 @@ while True:
     coin_no = 0
     coin_img = make_coin_img(image, circles)
 
+    for img in coin_img:
+        title = str(coin_no) + "img"
+        cv2.namedWindow(title)
+        cv2.moveWindow(title, (coin_no % 5) * 150, (coin_no // 5) * 150)
+        cv2.imshow(title, img)
+        coin_no += 1
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     for center, radius in circles:
         count += 1
         text = (center[0] - radius * 1 // 10 - 15, center[1]+10)
