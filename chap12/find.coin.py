675b32c548ec0fdd3d2385c5f79969a0e28c20ca
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-23 04:55:42 +0900

    [Feat] Chap12 Find Coin

---
+++ chap12/find.coin.py
@@ -0,0 +1,33 @@
+import cv2
+from header.coin_preprocess import *
+
+imageNum = 0
+
+while True:
+    cv2.destroyAllWindows()
+    image, binary = preprocessing(imageNum)
+    if image is None: raise Exception("None Image")
+
+    count = 0
+
+    circles = find_coins(binary)
+
+    coin_no = 0
+    coin_img = make_coin_img(image, circles)
+
+    for center, radius in circles:
+        count += 1
+        text = (center[0] - radius * 1 // 10 - 15, center[1]+10)
+        print(count, " - radius : ", radius)
+        cv2.circle(image, center, radius, (0, 255, 0), 2)
+
+    cv2.putText(image, (f'./images/coin/{imageNum:02d}.jpg'), (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(image, (f'Coin : {count}'), (5, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.imshow(f'Coin_{imageNum:02d}', image)
+
+    # cv2.imshow("Binary", binary)
+    Key = cv2.waitKey(0)
+
+    if Key == 27: break;
+    elif Key == 2 & Key > 0: imageNum -= 1
+    elif Key == 3 & Key < 86: imageNum += 1
