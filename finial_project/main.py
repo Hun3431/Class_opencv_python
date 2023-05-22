30df9a3281cfd9a0a38bd3582d1f0589543cdc4b
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-23 05:08:45 +0900

    [Feat] Finial Project : Average Image

--- finial_project/main.py
+++ finial_project/main.py
@@ -11,3 +11,14 @@ for i in range(image_count):
     image = cv2.imread(f"./face_img/train/train{i:03d}.jpg", cv2.IMREAD_GRAYSCALE)
     image = cv2.resize(image, (width, height))
     image_files.append(image)
+
+sum_image = np.zeros((height, width), dtype=np.float32)
+
+for i in range(len(image_files)):
+    sum_image += np.array(image_files[i], dtype=np.float32)
+
+average = sum_image / image_count
+
+cv2.imshow("Average Image", average.astype(np.uint8))
+
+cv2.waitKey()
